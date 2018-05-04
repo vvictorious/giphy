f8a8e7a53a8ea09f893bd8ccb8c2bff12f0c871b
vvictorious <vvictorious30@gmail.com>
2018-05-04 08:50:47 -0600

    got the submit buttone to give an alert of what was typed in utilizing state

--- giphy-react/src/Search.js
+++ giphy-react/src/Search.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 
-class Search extends Component {
+class Search extends React.Component {
+	constructor(props){
+		super(props);
+		this.state={value: ''};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(event){
+		this.setState({value: event.target.value});
+	}
+	handleSubmit(event){
+		alert('A name was submitted: ' + this.state.value);
+		event.preventDefault();
+	}
+
+
+
 	render(){
 		return(
-			<form onSubmit={this.props.handleSubmit}>
+			<form onSubmit={this.handleSubmit}>
 				<input type="text"
-					value={this.props.handleChange}
+					value={this.state.value}
+					onChange={this.handleChange}
 					/>
 			<input type="submit" value="Search"/>
 			</form>
