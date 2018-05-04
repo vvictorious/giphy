9c7deea73fc55bdfffb8a756d4329f0048d47ebe
vvictorious <vvictorious30@gmail.com>
2018-05-03 22:04:38 -0600

    jk hello world is up and running now

--- giphy-react/src/Home.js
+++ giphy-react/src/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
       <div>
@@ -14,4 +14,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Home;
